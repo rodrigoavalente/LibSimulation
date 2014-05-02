5b81b8c5dce39860933cfdb64548fee160c00167
Rodrigo Valente <rodrigoavrav@gmail.com>
2014-05-01 22:55:34 -0300

    Teste do Hardlink 2

--- matrix.h
+++ matrix.h
@@ -1,20 +1,26 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+
+//Teste Hardlink
+
 class Matrix
 {
 private:
     int rows, cols;
     float **Mat;
+
 public:
     Matrix(); //Construtor
     Matrix(int row, int col);
     Matrix(const Matrix & otherMatrix);
     ~Matrix();
 
+
     void init(int row, int col); //Inicializador da Matriz
     void add(int rol, int col, float number);
     void print();
+
 
     Matrix operator+(Matrix Mat1);
     Matrix operator-(Matrix Mat1);
