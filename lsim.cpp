d32c20ea8cad55524ee66050b3feccfe7d71f697
Rodrigo Valente <rodrigoavrav@gmail.com>
2014-05-21 17:26:41 -0300

    Atualizada a biblioteca refente a última aula de sábado, e início da implementação de leitura de arquivos

--- lsim.cpp
+++ lsim.cpp
@@ -1,100 +1,147 @@
 #include "lsim.h"
 
-lsim::lsim()
+Lsim::Lsim()
 {
+
 }
 
-void lsim::addIO(Matrix in, Matrix out)
+void Lsim::addIO(Matrix in, Matrix out)
 {
-    this->input = in;
-    this->output = out;
+    this->u = in;
+    this->y = out;
 }
 
-bool lsim::VefMat(Matrix Mat1)
+void Lsim::addIO(const char *namefile)
 {
-    bool vef;
+    //char *name = const_cast<char*>(namefile.c_str());
+    ifstream myfile(namefile);
+    string data;
 
-    if((Mat1.getRows() != 0) && (Mat1.getCols() != 0))
-        vef = true;
+
+    try
+    {
+        if (!myfile.is_open())
+            throw "Nao foi possivel abrir o arquivo";
+        else
+        {
+            int posBar;
+            string input = "", output = "", time = "";
+
+
+            while(getline( myfile, data))
+            {
+                myfile>>data;
+                posBar = data.find("-");
+                input = input + data.substr( 0, posBar) + ";";
+                data.erase( 0, posBar+1);
+                posBar = data.find("-");
+                output = output + data.substr(0, posBar) + ";";
+                data.erase( 0, posBar+1);
+                time = time + data + ";";
+                data.erase(0, time.length());
+            }
+            this->u = input;
+            this->y = output;
+            this->PeriodicTime = time;
+            this->u.print();
+            this->y.print();
+            this->PeriodicTime.print();
+        }
+
+    }
+    catch(const char* msg)
+    {
+        cerr<<msg<<endl;
+    }
+
+
+
+}
+
+void Lsim::modelCoef(Matrix coef)
+{
+    this->X = coef;
+}
+void Lsim::polyModel(int grau)
+{
+    this->Model = "polinomial";
+    this->u.print();
+    this->A.ones(this->u.getRows(),1);
+    for(int i = 1; i <= grau; i++)
+        this->A = this->A|(this->u>i);
+}
+
+void Lsim::simPoly(int grau)
+{
+    this->polyModel(grau);
+    this->b = this->A*this->X;
+}
+
+void Lsim::polyCoef(int grau)
+{
+    this->polyModel(grau);
+    this->X = ((~this->A*this->A)^-1)*(~this->A)*this->y;
+}
+
+//Modelos ARX (Auto Recursive with eXogenous output)
+
+void Lsim::arxModel(int ny, int nu)
+{
+    int maxNuNy,minRowInOut;
+    this->Model = "ARX";
+
+    if(ny>nu)
+        maxNuNy = ny;
     else
-        vef = false;
+        maxNuNy = nu;
 
-    return vef;
-}
+    if(this->y.getRows() > this->u.getRows())
+        minRowInOut = this->u.getRows();
+    else
+        minRowInOut = this->y.getRows();
 
-//void lsim::addIO(string FileName, int nIn, int nOut)
-//{
-//    FILE *pFile;
-//    pFile = fopen(FileName, "r");
-//    if (pFile != NULL)
-//        for(int i = 0; i < nIn; i++)
-//            for(int j = 0; j < nIn; j++)
-
-//        for(int i = nIn +1; i < (nIn + nOut); i++)
-//            for(int j = 0; j < (nIn + nOut); j++)
-//}
-
-void lsim::AddModelCoef(Matrix Coef)
-{
-    this->X = Coef;
-}
-
-void lsim::polyModel(int grau)
-{
-    try
+    for(int i = maxNuNy; i < minRowInOut; i++)
     {
-        if(!this->VefMat(this->input))
-            throw "A entrada nao foi adicionada";
-        else
-            this->Model = "Polynomial";
-            this->A.ones(this->input.getRows(), 1);
-            for (int i = 1; i<= grau; i++)
-                this->A = this->A|(this->input>i);
-    }
-    catch (const char* msg)
-    {
-        cerr<<msg<<endl;
-    }
-
-}
-
-void lsim::SimPoly(int grau)
-{
-    try
-    {
-        if(!this->VefMat(this->X))
-            throw "Os coeficientes nao foram adicionados";
-        else
+        this->b.add(i-maxNuNy+1,1,this->y.getMat( i, 1));
+        for(int j = 0; j < nu+ny; j++)
         {
-            this->polyModel(grau);
-            this->SimulatedOut = this->A*this->X;
+            if(j<ny)
+                this->A.add(i-maxNuNy+1,j+1,-this->y.getMat( i-ny+j,1));
+            else
+                this->A.add(i-maxNuNy+1,j+1,this->u.getMat( i-nu-ny+j, 1));
         }
     }
-    catch (const char* msg)
-    {
-        cerr<<msg<<endl;
-    }
 
 }
-
-void lsim::PolyCoefFind(int grau)
+void Lsim::simArx(int ny, int nu)
 {
-    try
-    {
-        if (!this->VefMat(this->A))
-        {
-            this->polyModel(grau);
-            this->PolyCoefFind(grau);
-        }
-        else if (!this->VefMat(this->output))
-                throw "Nao parametros suficientes para calcular os coeficientes";
-             else
-                this->X = ((~this->A*this->A)^-1)*((~this->A)*this->output);
-    }
-    catch (const char* msg)
-    {
-        cerr<<msg<<endl;
-    }
+    this->arxModel(ny,nu);
+    this->b = this->A*this->X;
 }
 
+void Lsim::arxCoef(int ny, int nu)
+{
+    this->arxModel(ny,nu);
+    this->X = ((~this->A*this->A)^-1)*(~this->A)*this->b;
+}
 
+void Lsim::eqdifModel(float h)
+{
+    Matrix dy, d2y;
+    dy = diff(this->y,h);
+    d2y = diff(dy,h);
+
+    for(int i = 0; i < d2y.getRows(); i++)
+    {
+        this->A.add(i+1,1,-this->y.getMat( i, 1));
+        this->A.add(i+1,2,-dy.getMat( i, 1));
+        this->A.add(i+1,3,this->u.getMat( i, 1));
+    }
+    this->b = d2y;
+}
+
+void Lsim::eqdifCoef(float h)
+{
+    this->eqdifModel(h);
+    this->X = ((~this->A*this->A)^-1)*(~this->A)*this->b;
+}
--- lsim.h
+++ lsim.h
@@ -2,23 +2,31 @@
 #define LSIM_H
 
 #include "matrix.h"
+#include "fstream"
 
-class lsim
+class Lsim
 {
-private:
-    Matrix output, input, A, X, SimulatedOut;
-    string Model;
+    public:
+        Matrix y,u,A,X,b, PeriodicTime;//Saídas,Entradas,Matriz montada a partir dos dados, coeficientes, Saída simulada
+        string Model;
 
-public:
-    lsim();
-    void addIO(Matrix in, Matrix out);
-//    void addIO(string FileName, int nIn, int nOut);
-    void AddModelCoef(Matrix Coef);
-    bool VefMat(Matrix Mat1);
+    public:
+        Lsim();
+        void addIO(Matrix in, Matrix out);
+        void addIO(const char *namefile);
+        void modelCoef(Matrix coef);
+    //Modelos polinomiais
+        void polyModel(int grau);
+        void simPoly(int grau);
+        void polyCoef(int grau);
+    //Modelos ARX (Auto Recursive with eXogenous output)
+        void arxModel(int ny, int nu);
+        void simArx(int ny, int nu);
+        void arxCoef(int ny, int nu);
+    //Modelos de equações diferenciais
+        void eqdifModel(float h);
+        void eqdifCoef(float h);
 
-    void polyModel(int grau);
-    void SimPoly(int grau);
-    void PolyCoefFind(int grau);
 };
 
 #endif // LSIM_H
